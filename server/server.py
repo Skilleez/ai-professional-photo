ac13f246c5ceb015187e0c454634a194b22263e2
Abdullah <abdullah.syed@student.unsw.edu.au>
2025-03-05 02:11:36 +1100

    CORS trying

--- server/server.py
+++ server/server.py
@@ -1,19 +1,13 @@
 from fastapi import FastAPI, Header, Request
 from image import image
 from aws import upload_image, get_presigned
-from fastapi.middleware.cors import CORSMiddleware
+from starlette.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 import json
 from mongodb import store_url, images
 
 app = FastAPI()
 
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["*"],
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
 # sample = {'order_status_code': 200, 'order_status': 'order_complete', 
 #           'output_urls': ['https://photai-s3-bucket.apyhi.com/background_generator/output_image/2025-03-03/hmxf4ey_2025-03-03T07:17:55.906Z_output_0.webp', 'https://photai-s3-bucket.apyhi.com/background_generator/output_image/2025-03-03/hmxf4ey_2025-03-03T07:17:55.906Z_output_1.webp', 'https://photai-s3-bucket.apyhi.com/background_generator/output_image/2025-03-03/hmxf4ey_2025-03-03T07:17:55.906Z_output_2.webp', 'https://photai-s3-bucket.apyhi.com/background_generator/output_image/2025-03-03/hmxf4ey_2025-03-03T07:17:55.906Z_output_3.webp']}
 
@@ -37,3 +31,10 @@ async def get_images(x_user_id: str = Header(None)):
   json_list = json.dumps(list)
   print(json_list)
   return json_list
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
