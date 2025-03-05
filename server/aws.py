41af5b9f9f349c3aaab924bec28632ee905bc252
Abdullah <abdullah.syed@student.unsw.edu.au>
2025-03-06 00:43:00 +1100

    Fixed env variables

--- server/aws.py
+++ server/aws.py
@@ -1,7 +1,11 @@
 import boto3
 import uuid
+from dotenv import load_dotenv
+import os
 
-s3 = boto3.client('s3')
+load_dotenv()
+
+s3 = boto3.client('s3', aws_access_key_id=os.getenv("AWS_ACCESS_KEY_ID"), aws_secret_access_key=os.getenv("AWS_SECRET_ACCESS_KEY"), region_name=os.getenv("AWS_REGION"))
 bucket = "myawsbucketinsydneyforuni"
 
 def upload_image(user_id, image):  
